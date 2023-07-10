38cce3d164ed8970ed02de83cb8f9a57650d35aa
jchaz22 <jason.chaskin@gmail.com>
2023-07-10 16:23:18 -0400

    02_SMART_CONTRACT

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,12 +5,16 @@ import "forge-std/Script.sol";
 import {ArbitrumShield} from "src/ArbitrumShield.sol";
 
 contract DeployScript is Script {
+    bytes16 private constant APP_ID = 0x4895f3f40962d377e5119f5a5e67ca5d;
+    bool private constant IS_IMPERSONATION_MODE = true;
+
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        console.log(deployerPrivateKey);
         vm.startBroadcast(deployerPrivateKey);
         ArbitrumShield arbitrumShield = new ArbitrumShield(
-            "ipfs://bafybeihehkiq5e46fpt2zwemf7jtaqb75zfuxw24vlscwxql5ifkuqbfgy/"
+            "ipfs://bafybeihehkiq5e46fpt2zwemf7jtaqb75zfuxw24vlscwxql5ifkuqbfgy/",
+            APP_ID,
+            IS_IMPERSONATION_MODE
         );
         console.log("NFT Contract deployed at", address(arbitrumShield));
         vm.stopBroadcast();
--- src/ArbitrumShield.sol
+++ src/ArbitrumShield.sol
@@ -2,16 +2,38 @@
 pragma solidity ^0.8.19;
 
 import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "sismo-connect-solidity/SismoLib.sol";
 
-contract ArbitrumShield is ERC721 {
+contract ArbitrumShield is ERC721, SismoConnect {
+    using SismoConnectHelper for SismoConnectVerifiedResult;
+
     uint256 private _tokenId;
     string private _baseTokenURI;
 
-    constructor(string memory baseTokenURI) ERC721("ArbitrumShield", "AS") {
+    bytes16 private constant COINBASE_SHIELD_GROUP_ID =
+        0x842e4d1671d72526762a77ade9feb49a;
+
+    constructor(
+        string memory baseTokenURI,
+        bytes16 appId,
+        bool isImpersonationMode
+    )
+        ERC721("ArbitrumShield", "AS")
+        SismoConnect(buildConfig(appId, isImpersonationMode))
+    {
         _baseTokenURI = baseTokenURI;
     }
 
-    function mint() public {
+    function verifySismoConnectResponse(bytes memory response) public {
+        SismoConnectVerifiedResult memory result = verify({
+            responseBytes: response,
+            auth: buildAuth({authType: AuthType.VAULT}),
+            claim: buildClaim({
+                groupId: COINBASE_SHIELD_GROUP_ID,
+                claimType: ClaimType.GTE
+            }),
+            signature: buildSignature({message: abi.encode(msg.sender)})
+        });
         _safeMint(msg.sender, _tokenId);
         _tokenId++;
     }
@@ -25,5 +47,9 @@ contract ArbitrumShield is ERC721 {
     ) public view virtual override returns (string memory) {
         _requireMinted(tokenId);
         return _baseTokenURI;
+    }
+
+    function getCoinbaseShieldGroupId() public pure returns (bytes16) {
+        return COINBASE_SHIELD_GROUP_ID;
     }
 }
